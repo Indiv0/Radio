39fab00e4f5504e27faca07e74be13d15a6a226d
Indiv0 <indiv0soft@gmail.com>
2012-12-06 00:46:11 -0500

    Added DoubleConstructorFactory

---
+++ src/main/java/org/github/indiv0/radio/main/DoubleConstructorFactory.java
@@ -0,0 +1,10 @@
+package org.github.indiv0.radio.main;
+
+import ashulman.mbapi.util.ConstructorFactory;
+
+public class DoubleConstructorFactory extends ConstructorFactory<Double> {
+    @Override
+    public Double get() {
+        return 0.0;
+    }
+}
