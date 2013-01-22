bb462d954d790770c5bbc68e409af325a46635a8
Indiv0 <indiv0soft@gmail.com>
2013-01-21 19:13:46 -1000

    Removed BukkitUtils, updated to latest Mbapi

--- src/main/java/com/github/indiv0/radio/commands/CommandRadio.java
+++ src/main/java/com/github/indiv0/radio/commands/CommandRadio.java
@@ -1,15 +1,12 @@
 package com.github.indiv0.radio.commands;
 
-import org.bukkit.command.CommandSender;
+import ashulman.mbapi.commands.DelegatingCommand;
 
 import com.github.indiv0.radio.commands.radio.CommandOff;
 import com.github.indiv0.radio.commands.radio.CommandScan;
 import com.github.indiv0.radio.commands.radio.CommandTune;
 import com.github.indiv0.radio.util.Commands;
 import com.github.indiv0.radio.util.RadioConfigurationContext;
-
-import ashulman.mbapi.commands.DelegatingCommand;
-import ashulman.typesafety.TypeSafeList;
 
 public class CommandRadio extends DelegatingCommand {
 
@@ -18,11 +15,5 @@ public class CommandRadio extends DelegatingCommand {
         registerSubcommand(new CommandTune(configurationContext));
         registerSubcommand(new CommandScan(configurationContext));
         registerSubcommand(new CommandOff(configurationContext));
-    }
-
-    @Override
-    public TypeSafeList<String> onTabComplete(CommandSender sender, TypeSafeList<String> args) {
-        // TODO Auto-generated method stub
-        return null;
     }
 }
