2b5b18563426ab85ff02d11f6b62c4c270f1a5df
Andy Shulman <andy.shulman@hotmail.com>
2013-03-17 01:57:34 -0700

    Add code to load radios for any players already on the server

--- src/main/java/com/github/indiv0/radio/management/RadioInfoManager.java
+++ src/main/java/com/github/indiv0/radio/management/RadioInfoManager.java
@@ -2,10 +2,8 @@ package com.github.indiv0.radio.management;
 
 import java.math.BigDecimal;
 
-import com.github.indiv0.radio.serialization.Frequency;
-import com.github.indiv0.radio.serialization.Radio;
-import com.github.indiv0.radio.util.FrequencyConstructorFactory;
-import com.github.indiv0.radio.util.SupplimentaryTypes;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import ashulman.mbapi.management.InfoManager;
 import ashulman.mbapi.storage.TypeSafeStorageMap;
@@ -13,12 +11,17 @@ import ashulman.mbapi.storage.TypeSafeStorageSet;
 import ashulman.mbapi.util.ConfigurationContext;
 import ashulman.typesafety.TypeSafeSet;
 
+import com.github.indiv0.radio.serialization.Frequency;
+import com.github.indiv0.radio.serialization.Radio;
+import com.github.indiv0.radio.util.FrequencyConstructorFactory;
+import com.github.indiv0.radio.util.SupplimentaryTypes;
+
 public class RadioInfoManager extends InfoManager {
 
     private final TypeSafeStorageMap<Frequency> frequencies;
     private final TypeSafeStorageSet<Radio> radios;
 
-    private final FrequencyConstructorFactory FREQUENCY_FACTORY = new FrequencyConstructorFactory();
+    private static final FrequencyConstructorFactory FREQUENCY_FACTORY = new FrequencyConstructorFactory();
 
     public RadioInfoManager(ConfigurationContext configurationContext) {
         super(configurationContext);
@@ -26,10 +29,13 @@ public class RadioInfoManager extends InfoManager {
         // Retrieves the storage map for "frequencies" and "radios".
         frequencies = storageManager.getStorageMap("frequencies", SupplimentaryTypes.FREQUENCY);
         radios = storageManager.getStorageSet("radios", SupplimentaryTypes.RADIO);
-
+        registerPlayerInfoLoader(frequencies, FREQUENCY_FACTORY);
         radios.load();
 
-        registerPlayerInfoLoader(frequencies, FREQUENCY_FACTORY);
+        // load any players already on the server -- in case of reload
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            frequencies.load(player.getName(), FREQUENCY_FACTORY);
+        }
     }
 
     public TypeSafeStorageSet<Radio> getRadios() {
